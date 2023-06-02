64d06608f1c82041c7f196b0381e06666f5b0d4a
Guilherme Borges <guilhermepardi@gmail.com>
2023-06-01 22:27:29 -0300

    feat: components Box, Text, Heading e Avatar

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde facilis, alias deleniti iusto voluptatem numquam nemo, voluptate nisi optio, odit eos esse. Voluptatibus qui maxime suscipit doloribus aliquam officia. Doloribus.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,31 +1,4 @@
-import { ComponentProps } from 'react'
-import { styled } from './styles'
-
-export const Button = styled('button', {
-  fontFamily: '$code',
-  backgroundColor: '$ignite300',
-  borderRadius: '$md',
-  border: '0',
-  fontWeight: 'bold',
-  color: '$white',
-
-  variants: {
-    size: {
-      big: {
-        fontSize: '$16',
-        padding: '$3 $6',
-      },
-
-      small: {
-        fontSize: '$14',
-        padding: '$2 $4',
-      },
-    },
-  },
-
-  defaultVariants: {
-    size: 'small',
-  },
-})
-
-export type ButtonProps = ComponentProps<typeof Button>
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar'
