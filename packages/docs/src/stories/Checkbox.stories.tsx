20ac7faf4c618471d91045922cebd0198dc52e40
Guilherme Borges <guilhermepardi@gmail.com>
2023-06-05 13:20:59 -0300

    feat: components in Storybook Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,25 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, Box, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          <Text>Accept terms of use</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {},
+}
