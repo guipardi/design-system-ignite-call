46efdfba8d9666369fe291e8b909c8336c7aeece
Guilherme Borges <guilhermepardi@gmail.com>
2023-06-05 15:39:11 -0300

    subindo comentários de criando um array

--- packages/react/src/components/MultiStep/index.tsx
+++ packages/react/src/components/MultiStep/index.tsx
@@ -19,3 +19,12 @@ export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
     </MultiStepContainer>
   )
 }
+
+/* 
+  {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+    return <Step key={step} active={currentStep >= step} />
+  })}
+
+  Nesse trecho de código a gente basicamente cria um array [1, 2, 3, 4]
+  Depois percorremos todos com um map, e partir deles renderizo um componente Step
+*/
