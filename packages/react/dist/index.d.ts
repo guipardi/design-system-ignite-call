343f1e1b838ec8b85439ad13cbd8284f2ee619c8
Guilherme Borges <guilhermepardi@gmail.com>
2023-06-05 14:23:02 -0300

    feat: component Multistep in storybook

--- packages/react/dist/index.d.ts
+++ packages/react/dist/index.d.ts
@@ -1663,4 +1663,10 @@ interface CheckboxProps extends ComponentProps<typeof CheckboxContainer> {
 }
 declare function Checkbox(props: CheckboxProps): react_jsx_runtime.JSX.Element;
 
-export { Avatar, AvatarProps, Box, BoxProps, Button, ButtonProps, Checkbox, CheckboxProps, Heading, HeadingProps, Text, TextInput, TextInputProps, TextProps, Textarea, TextareaProps };
+interface MultiStepProps {
+    size: number;
+    currentStep?: number;
+}
+declare function MultiStep({ size, currentStep }: MultiStepProps): react_jsx_runtime.JSX.Element;
+
+export { Avatar, AvatarProps, Box, BoxProps, Button, ButtonProps, Checkbox, CheckboxProps, Heading, HeadingProps, MultiStep, MultiStepProps, Text, TextInput, TextInputProps, TextProps, Textarea, TextareaProps };
